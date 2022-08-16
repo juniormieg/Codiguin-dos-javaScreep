57e5e9b8abf822f91218f6703e2ebb8fc62e935b
juniormieg <junior14@alu.ufc.br>
2022-08-16 14:03:21 -0300

    Detalhes extras

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function mostraSenha() {
+    let login = document.getElementById("login").value
     let password = document.getElementById("password").value
     let nome = document.getElementById("name")
     console.log(password)
-    nome.innerHTML = `sua senha é: ${password}`
+    nome.innerHTML = `seu login é: ${login}, sua senha é: ${password}`
 }
