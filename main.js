afbc89fa4cb2c7bbd10b2d95058d2f31e2d0ad45
juniormieg <junior14@alu.ufc.br>
2022-08-15 23:50:40 -0300

    Coisas simples

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-console.log("vinisu é muito boi")
-const variavelA = 10
-let variavelB = 10
-// variavelB = 20
-console.log("o valor deu: ", variavelA - variavelB)
-function mostraNome() {
-    let nickname = document.getElementById("nickname").value
+function mostraSenha() {
+    let password = document.getElementById("password").value
     let nome = document.getElementById("name")
-    console.log(nickname)
-    // let noTexto = document.createTextNode(nickname)
-    // nome.innerHTML = "Seu nick e: " + nickname 
-    nome.innerHTML = `vinisu é um merda ${nickname} vacilão`
+    console.log(password)
+    nome.innerHTML = `sua senha é: ${password}`
 }
